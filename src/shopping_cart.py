c84805ad2a5fb02a07f796bdf7ee28110699958f
Danil Sobilyanskiy <SobilyanskiyDanil@iclod.com>
2026-05-23 15:36:41 +0300

    lesson 3 v.1.1

---
+++ src/shopping_cart.py
@@ -0,0 +1,9 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items = {}
+
+    def add_item(self, item_name, price):
+        self.items[item_name] = price
+
+    def get_total_price(self):
+        return sum(self.items.values())
